5afdcaca8ae3199a1a4ed3dc4150fe4d6af1e500
Niharika Mehandiratta <49205185+NiharikaMehandiratta29@users.noreply.github.com>
2019-04-06 23:50:09 +0530

    Update MULTITHREAD.c
    Added a writer function which will allow the writer processed to change the value of share variable.

--- MULTITHREAD.c
+++ MULTITHREAD.c
@@ -1,27 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h> 
+#include<stdlib.h>
 #include<unistd.h>
 #include<conio.h>
 #include<pthread.h>
-pthread_mutex_t lock;
+#include<semaphore.h
+sem_t wrt , mutex;
 int shared = 0;
-void *thread_function(void *args)
+
+
+
+void *writer(void *args)
 {
-	pthread_mutex_lock(&lock);
-	shared++; //critical condition
-	pthread_mutex_unlock(&lock);
+	int id;
+	id = ((int) args);
+	sem_wait(&wrt);
+	shared++;
+	printf("The writer process %d has updated the shared value to %d\n",id+1,shared);
+	sem_post(&wrt);
+
 }
+
 int main()
-{	
-	pthread_t thread[5];
+{
+	pthread_t   write_thread[5];
+	sem_init(&wrt , 0 , 1);
+
 	int i;
 	for(i = 0 ; i< 5 ; i++)
 	{
-		pthread_create(&thread[i], NULL , thread_function , NULL);
+		pthread_create(&write_thread[i], NULL , writer , (void *) i );
 	}
 	for(i = 0 ; i< 5 ; i++)
 	{
-		pthread_join(thread[i], NULL);
+		pthread_join(write_thread[i], NULL);
 	}
 	return 0;
 }
