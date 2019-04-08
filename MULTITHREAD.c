a78efbde43cd4a4f211749cb0949367fb3195afc
Niharika Mehandiratta <49205185+NiharikaMehandiratta29@users.noreply.github.com>
2019-04-08 20:46:55 +0530

    pthread_mutex_lock used

--- MULTITHREAD.c
+++ MULTITHREAD.c
@@ -3,8 +3,8 @@
 #include<unistd.h>
 #include<conio.h>
 #include<pthread.h>
-#include<semaphore.h
-sem_t wrt , mutex;
+
+pthread_mutex_t wrt  , mutex;
 int shared = 0, readCount = 0;
 
 
@@ -13,10 +13,10 @@ void *writer(void *args)
 {
 	int id;
 	id = ((int) args);
-	sem_wait(&wrt);
+	pthread_mutex_lock(&wrt);
 	shared++;
 	printf("The writer process %d has updated the shared value to %d\n",id+1,shared);
-	sem_post(&wrt);
+	pthread_mutex_unlock(&wrt);
 
 }
 
@@ -24,30 +24,31 @@ void *reader(void *args)
 {
 	int id;
 	id = ((int) args);
-	sem_wait(&mutex);
+	pthread_mutex_lock(&mutex);
 	readCount++;
 	if(readCount == 1) // the first reader will not allow the writers to enter the critical condition
 	{
-		sem_wait(&wrt);
+		pthread_mutex_lock(&wrt);
 	}
-	sem_post(&mutex);
+	pthread_mutex_unlock(&mutex);
 	printf("The reader process %d has entered the critical condition and the shared value is %d\n" , id+1,shared);
 
 	sleep(1);
 
-	sem_wait(&mutex);
+	pthread_mutex_lock(&mutex);
 	readCount--;
 	if(readCount==0)
 	{
-		sem_post(&wrt);
+		pthread_mutex_unlock(&wrt);
 	}
-	sem_post(&mutex);
+	pthread_mutex_unlock(&mutex);
 
 }
 int main()
 {
-	pthread_t   write_thread[5];
-	sem_init(&wrt , 0 , 1);
+	pthread_t read_thread[5],  write_thread[5];
+    pthread_mutex_init(&wrt,NULL);
+	pthread_mutex_init(&mutex,NULL);
 
 	int i;
 	for(i = 0 ; i< 5 ; i++)
@@ -57,8 +58,10 @@ int main()
 	}
 	for(i = 0 ; i< 5 ; i++)
 	{
-		pthread_join(write_thread[i], NULL);
+		pthread_join(read_thread[i], NULL);
 		pthread_join(write_thread[i], NULL);
 	}
+	pthread_mutex_destroy(&wrt);
+	pthread_mutex_destroy(&mutex);
 	return 0;
 }
